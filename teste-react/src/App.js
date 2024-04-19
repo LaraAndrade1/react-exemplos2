422d520556832a1d62c0746681e419b981a882d2
LaraAndrade1 <130664603+LaraAndrade1@users.noreply.github.com>
2024-04-19 10:00:57 -0300

    .

---
+++ teste-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World React</h1>
+      <button className='button'>clique em mim!</button>
+    </div>
+  );
+}
+
+export default App;
